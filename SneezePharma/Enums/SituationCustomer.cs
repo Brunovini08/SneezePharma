ae17dbbc88404cab6d98618001937912c5828040
Brunovini08 <brunovinicius082005@gmail.com>
2025-10-23 16:10:01 -0300

    feat: Adicionando as classes de Cliente e Cliente Restrito

---
+++ SneezePharma/Enums/SituationCustomer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneezePharma.Enums
+{
+    public enum SituationCustomer
+    {
+        A=1,
+        I=2
+    }
+}
--- SneezePharma/Program.cs
+++ SneezePharma/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SneezePharma.Enums;
+using SneezePharma.Models;
+
+Customer customer = new Customer("47998764813", "Bruno", new DateOnly(), "16993559811", new DateOnly(), SituationCustomer.A);
+
