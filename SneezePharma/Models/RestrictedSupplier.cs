e07d8b4319fd718b0c0ea6ba95d09a6c4350fe00
jufrancic <juliafrancictostes@gmail.com>
2025-10-23 16:04:44 -0300

    feat: criando classe Supplier e RestrictedSupplier

---
+++ SneezePharma/Models/RestrictedSupplier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneezePharma.Models
+{
+    public class RestrictedSupplier
+    {
+        public List<Supplier> Restrictedsuppliers { get; } = new List<Supplier>();
+        public void Adicionar()
+        {
+
+        }
+    }
+}
