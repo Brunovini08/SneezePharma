3bf880b19ca14d5e7bd6957f9db29a38c8e255eb
GustavoRF1 <gustavo13rocha@gmail.com>
2025-10-23 14:38:51 -0300

    Validador CDB

---
+++ SneezePharma/Models/Medicine.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneezePharma.Models
+{
+    public class Medicine
+    {
+        public string CDB { get; private set; }
+        public string Nome { get; private set; }
+        public char Categoria { get; private set; }
+        public decimal ValorVenda { get; private set; }
+        public DateOnly UltimaVenda { get; private set; }
+        public DateOnly DataCadastro { get; private set; }
+        public char Situacao { get; private set; }
+
+        public Medicine(string cdb, string nome, char categoria, decimal valorVenda, DateOnly ultimaVenda, DateOnly dataCadastro, char situacao)
+        {
+            CDB = cdb;
+            Nome = nome;
+            Categoria = categoria;
+            ValorVenda = valorVenda;
+            UltimaVenda = ultimaVenda;
+            DataCadastro = dataCadastro;
+            Situacao = situacao;
+        }
+
+        public bool ValidarCDB(string cdb)
+        {
+            int[] resultado = new int[13];
+            resultado = cdb.Select(c => int.Parse(c.ToString())).ToArray();
+            if (resultado[0] != 7 || resultado[1] != 8 || resultado[2] != 9)
+            {
+                Console.WriteLine("Os três primeiros dígitos devem ser especificamente: 789");
+                return false;
+            }
+            else
+            {
+                int somaImpar = 0, somaPar = 0;
+                for (int i = 0; i < 12; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        somaImpar += resultado[i];
+                    }
+                    else
+                    {
+                        somaPar += resultado[i];
+                    }
+                }
+                int somaTotal = somaImpar + (somaPar * 3);
+                int digitoVerificador = 10 - (somaTotal % 10);
+                if (digitoVerificador == 10)
+                {
+                    digitoVerificador = 0;
+                }
+                if (digitoVerificador == resultado[12])
+                {
+                    Console.WriteLine("Código de Barra válido");
+                    Console.WriteLine($"Digito Verificador: {digitoVerificador}");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Dígito verificador inválido.");
+                    Console.WriteLine($"Digito Verificador: {digitoVerificador}");
+                    return false;
+                }
+            }
+        }
+
+        public 
+
+    }
+}
--- SneezePharma/Program.cs
+++ SneezePharma/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using SneezePharma;
+using SneezePharma.Models;
+
+Console.WriteLine("Digite o código de barras: ");
+string cdb = Console.ReadLine();
+
+Medicine medicine = new Medicine(cdb, "", 'A', 0, DateOnly.MinValue, DateOnly.MinValue, 'A');
+
+bool valido = medicine.ValidarCDB(cdb);
+
