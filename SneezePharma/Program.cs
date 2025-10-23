88b6258e96f5ea5e05b44e2eee91c64e5acb5216
fesperli <fesperli@gmail.com>
2025-10-23 15:58:09 -0300

    feat: Criação da classe Produce

--- SneezePharma/Program.cs
+++ SneezePharma/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SneezePharma;
+using SneezePharma.Exceptions;
+using SneezePharma.ProduceItem;
+
+List <Produce> listamanipulacoes = new List<Produce> ();
+int opcao;
+
+
+
+
+
+
+
+
+
+
+
+
+do
+{
+    Console.WriteLine("\n 1 - Cadastrar Manipulação");
+    Console.WriteLine("2 - Listar Manipulações");
+    Console.WriteLine("4 - Sair");
+    Console.WriteLine("\n Escolha uma opção: ");
+    opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao) {
+        case 1:
+            var produce  = new Produce().CadastrarManipulacao();
+            if (produce != null)
+            {
+                listamanipulacoes.Add(produce);
+            }
+            break;
+        case 2:
+            
+            break;
+
+
+
+} while (opcao != 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
