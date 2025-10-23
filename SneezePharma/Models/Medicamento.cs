3dcd3e9eea0713108ac93a5d06656beaa4171ea5
GustavoRF1 <gustavo13rocha@gmail.com>
2025-10-23 13:54:17 -0300

    Começando dnv

---
+++ SneezePharma/Models/Medicamento.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneezePharma.Models
+{
+    public class Medicine
+    {
+        public string CDB { get; private set; }
+        public string Nome { get; private set; }
+        public char Categoria { get; private set; }
+        public decimal ValorVenda { get; private set; }
+        public DateOnly UltimaVenda { get; private set; }
+        public DateOnly DataCadastro { get; private set; }
+        public char Situacao { get; private set; }
+
+        public Medicine(string cdb, string nome, char categoria, decimal valorVenda, DateOnly ultimaVenda, DateOnly dataCadastro, char situacao)
+        {
+            CDB = cdb;
+            Nome = nome;
+            Categoria = categoria;
+            ValorVenda = valorVenda;
+            UltimaVenda = ultimaVenda;
+            DataCadastro = dataCadastro;
+            Situacao = situacao;
+        }
+
+
+    }
+}
