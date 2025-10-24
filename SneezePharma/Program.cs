9b258ddeafc352b16dd9f37b2e156bcf03c999de
fesperli <fesperli@gmail.com>
2025-10-24 10:34:32 -0300

    feat: fazendo merge das minhas classes para a master

--- SneezePharma/Program.cs
+++ SneezePharma/Program.cs
@@ -1,3 +1,55 @@
-using SneezePharma.Helpers;
+using SneezePharma;
+using SneezePharma.Models;
 
-Menu.MenuPrincipal();
+List <Produce> listamanipulacoes = new List<Produce> ();
+int opcao;
+
+
+
+
+
+
+
+
+
+
+
+
+do
+{
+    Console.WriteLine("\n 1 - Cadastrar Manipulação");
+    Console.WriteLine("2 - Listar Manipulações");
+    Console.WriteLine("4 - Sair");
+    Console.WriteLine("\n Escolha uma opção: ");
+    opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao) {
+        case 1:
+            var produce  = new Produce().CadastrarManipulacao();
+            if (produce != null)
+            {
+                listamanipulacoes.Add(produce);
+            }
+            break;
+        case 2:
+            
+            break;
+
+
+
+} while (opcao != 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
