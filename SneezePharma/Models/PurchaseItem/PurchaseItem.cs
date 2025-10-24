e688fb313355a94999dad53afbd7477348170095
GustavoRF1 <gustavo13rocha@gmail.com>
2025-10-24 09:19:37 -0300

    Finalizado Medicine

--- SneezePharma/Models/PurchaseItem/PurchaseItem.cs
+++ SneezePharma/Models/PurchaseItem/PurchaseItem.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SneezePharma.Models
+namespace SneezePharma.Models.PurchaseItem
 {
     public class PurchaseItem
     {
