630e1428c75f5f83536fe12546014a2020c3df34
GustavoRF1 <gustavo13rocha@gmail.com>
2025-10-23 21:35:19 -0300

    Criação das outras classes

---
+++ SneezePharma/Models/PurchaseItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneezePharma.Models
+{
+    public class PurchaseItem
+    {
+        public string IdCompra { get; private set; }
+        public string Ingrediente { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+        public decimal TotalItem { get; private set; }
+
+        public PurchaseItem(string idCompra, string ingrediente, int quantidade, decimal valorUnitario, decimal totalItem)
+        {
+            IdCompra = idCompra;
+            Ingrediente = ingrediente;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+            TotalItem = totalItem;
+        }
+    }
+}
---
+++ SneezePharma/Models/Purchases.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneezePharma.Models
+{
+    public class Purchases
+    {
+        public int IdCompra {  get; private set; }
+        public DateOnly DataCompra { get; private set; }
+        public decimal ValorTotal { get; private set; }
+
+        public Purchases(int idCompra, DateOnly dataCompra, decimal valorTotal)
+        {
+            IdCompra = idCompra;
+            DataCompra = dataCompra;
+            ValorTotal = valorTotal;
+        }
+
+    }
+}
